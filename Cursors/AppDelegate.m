4cbacf77b76d02c90aa536170d388f198f1eee26
Tim Guo <timguoqk@gmail.com>
2015-02-20 23:17:43 -0800

    Initial Commit

---
+++ Cursors/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Cursors
+//
+//  Created by Qikun Guo on 2/20/15.
+//  Copyright (c) 2015 Tim Guo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Cursors/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Cursors
+//
+//  Created by Qikun Guo on 2/20/15.
+//  Copyright (c) 2015 Tim Guo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
